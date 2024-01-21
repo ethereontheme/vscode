0f80275462ce7265c650984a9a15f506f148f504
Rohit Saluja <rohsaluj@cisco.com>
2024-01-21 20:37:34 +0530

    Add color templater script

---
+++ src/ethereon-color-theme.mjs
@@ -0,0 +1,10 @@
+import { readFileSync, write, writeFileSync } from 'fs';
+import * as jt from 'json-templater'
+
+const color_base = JSON.parse(readFileSync('src/base/src/base.json', 'utf8'));
+
+const theme_base = JSON.parse(readFileSync('src/themes/ethereon-color-theme.json', 'utf8'));
+
+const color_theme = jt.object(theme_base, color_base);
+
+writeFileSync('themes/ethereon-color-theme.json', JSON.stringify(color_theme, null, 4));
